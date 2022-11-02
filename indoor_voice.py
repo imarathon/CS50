593aadeec722bb03df68c151620402807c7877ba
imarathon <javier.huilca@gmail.com>
2022-11-01 20:39:01 -0700

    Adding a new file for CS50 assignmemnt indoor_voice

---
+++ indoor_voice.py
@@ -0,0 +1,6 @@
+#A program in Python that prompts the user for input and then outputs that same input in lowercase. 
+#Punctuation and whitespace should be outputted unchanged.
+
+user = input("Please enter some text: ").strip().lower()
+print("")
+print(user)
