2ea317027b755ef3019f745b9738841e9be7294d
imarathon <javier.huilca@gmail.com>
2022-11-14 11:26:16 -0800

    updated einstein program

---
+++ einstein.py
@@ -0,0 +1,12 @@
+# A program in Python that prompts the user for mass as an integer (in kilograms) 
+# and then outputs the equivalent number of Joules as an integer
+import math
+
+def main():
+    user = int(input("Please input a number in Kilograms: "))
+    joules = 300000000
+    result = user * (joules**2)
+
+    print(round(user), "Kilograms", "equals", result, "in Joules.")
+
+main()
