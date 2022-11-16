6f4db7ca5e36997e05302afe612878e92ad15a5a
imarathon <javier.huilca@gmail.com>
2022-11-16 13:00:01 -0800

    Updated compare.py and deep.py

---
+++ Conditionals/compare.py
@@ -0,0 +1,11 @@
+#Compare integers. Input from user
+
+x = int(input("What is x? : "))
+y = int(input("What is y? : "))
+
+if x < y: 
+    print("x is less than y")
+elif x > y: 
+    print("x is greater than y")
+else: 
+    print("x and y are equal")
