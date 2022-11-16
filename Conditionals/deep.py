95b96ce46868a6cc280cc36e358e338e1f890697
imarathon <javier.huilca@gmail.com>
2022-11-16 13:00:48 -0800

    deleted conditionals.py

---
+++ Conditionals/deep.py
@@ -0,0 +1,4 @@
+# A program that prompts the user for the answer to the Great Question of Life, 
+# the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) 
+# forty-two or forty two. Otherwise output No.
+
